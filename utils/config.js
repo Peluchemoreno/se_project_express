63e5de76b8ab29dab0f9a9e16bed82e044b29ff2
Justin McDonald <jmcdmoreno19@gmail.com>
2024-08-21 16:52:23 -0500

    update enviornment variables

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
-const JWT_SECRET = "the power within, the power within, THE POWER WITHIN.. yeah"
+const {JWT_SECRET = "the power within, the power within, THE POWER WITHIN.. yeah"} = process.env
 
 module.exports = JWT_SECRET
